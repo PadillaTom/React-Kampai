4954b072b7093405e7c515d27456518f7091c31d
Tomas Padilla <padillatomasagustin@gmail.com>
2024-03-17 15:43:40 +0100

    navbar - basic mobile and desktop nav

---
+++ src/Utils/website-data.js
@@ -0,0 +1,23 @@
+// Links:
+export const links = [
+	{
+		id: 1,
+		name: "Le Restaurant",
+		url: "/about",
+	},
+	{
+		id: 2,
+		name: "Menu",
+		url: "/menu",
+	},
+	{
+		id: 3,
+		name: "Carte des Vins",
+		url: "/wines",
+	},
+	{
+		id: 3,
+		name: "Contact",
+		url: "/contact",
+	},
+];
