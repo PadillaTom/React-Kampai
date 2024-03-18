0d2d23dbac038f9044f9a652de566b69aa45d328
Tomas Padilla <padillatomasagustin@gmail.com>
2024-03-18 14:58:24 +0100

    landing - comenzando CTA

--- src/_Library/Page/Sections/Landing/Banner/BannerFullScreenVideo.js
+++ src/_Library/Page/Sections/Landing/Banner/BannerFullScreenVideo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import useWindowDimensions from "../../../../../Components/Utils/useWindowDimensions";
+import BasicButton from "../../../../Buttons/BasicButton";
 
 function BannerFullScreenHero({ data, isVideo }) {
 	const { width } = useWindowDimensions();
@@ -37,16 +38,23 @@ function BannerFullScreenHero({ data, isVideo }) {
 			<div className="data-container">
 				<h2 className="main-text">{data.HEADER}</h2>
 				<h2 className="sub-text">{data.HEADER_2}</h2>
+				<BasicButton
+					text={"Voir Menu"}
+					vPadding={"0.5rem"}
+					hPadding={"2.5rem"}
+					marginTop={"2rem"}
+					isOutlined
+				></BasicButton>
 			</div>
 		</BannerFullScreenHeroContainer>
 	);
 }
 
 const BannerFullScreenHeroContainer = styled.main`
+	position: relative;
 	width: 100%;
 	height: 85vh;
 	background: var(--ColorBlack);
-	position: relative;
 
 	.overlay {
 		position: absolute;
@@ -93,11 +101,11 @@ const BannerFullScreenHeroContainer = styled.main`
 		flex-direction: column;
 		align-items: center;
 		justify-content: space-between;
-		height: 6rem;
 
 		h2 {
 			font-weight: 200;
 			white-space: nowrap;
+			margin-bottom: 1rem;
 		}
 	}
 
