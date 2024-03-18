6e4c62513daab3f9e2de2aff1e057851a614b006
Tomas Padilla <padillatomasagustin@gmail.com>
2024-03-18 14:31:39 +0100

    landing - some quick fixes

--- src/_Library/Page/Sections/Landing/Banner/BannerFullScreenVideo.js
+++ src/_Library/Page/Sections/Landing/Banner/BannerFullScreenVideo.js
@@ -1,127 +1,123 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 import useWindowDimensions from "../../../../../Components/Utils/useWindowDimensions";
 
-function BannerFullScreenHero({data, isVideo}) {
-    const {width} = useWindowDimensions();
-    const isFullScreen = (width > 980);
-    return (
-        <BannerFullScreenHeroContainer>
+function BannerFullScreenHero({ data, isVideo }) {
+	const { width } = useWindowDimensions();
+	const isFullScreen = width >= 980;
+	return (
+		<BannerFullScreenHeroContainer>
+			{/* Background */}
+			<div className="overlay"></div>
+			{isVideo ? (
+				<video
+					src={
+						isFullScreen
+							? data.VIDEOS.VIDEO_FULL_SCREEN
+							: data.VIDEOS.VIDEO_MOBILE
+					}
+					autoPlay
+					loop={true}
+					muted
+					id={"video"}
+				/>
+			) : (
+				<img
+					src={
+						isFullScreen
+							? data.IMAGES.IMAGE_FULL_SCREEN
+							: data.IMAGES.IMAGE_MOBILE
+					}
+					alt={"Website Landing Page"}
+					className={"banner-img"}
+				/>
+			)}
 
-            {/* Background */}
-            <div className="overlay"></div>
-            {
-                isVideo ?
-                    <video src={isFullScreen ?
-                        data.VIDEOS.VIDEO_FULL_SCREEN :
-                        data.VIDEOS.VIDEO_MOBILE}
-                           autoPlay
-                           loop={true}
-                           muted
-                           id={"video"}
-                    />
-                    :
-                    <img src={isFullScreen ?
-                        data.IMAGES.IMAGE_FULL_SCREEN :
-                        data.IMAGES.IMAGE_MOBILE}
-                         alt={"Website Landing Page"}
-                         className={"banner-img"}
-                    />
-            }
-
-            {/* Data */}
-            <div className="data-container">
-                <h2 className="main-text">
-                    {data.HEADER}
-                </h2>
-                <h2 className="sub-text">
-                    {data.HEADER_2}
-                </h2>
-            </div>
-
-        </BannerFullScreenHeroContainer>
-    );
+			{/* Data */}
+			<div className="data-container">
+				<h2 className="main-text">{data.HEADER}</h2>
+				<h2 className="sub-text">{data.HEADER_2}</h2>
+			</div>
+		</BannerFullScreenHeroContainer>
+	);
 }
 
 const BannerFullScreenHeroContainer = styled.main`
-  width: 100%;
-  height: 99vh;
-  background: var(--ColorBlack);
-  position: relative;
+	width: 100%;
+	height: 85vh;
+	background: var(--ColorBlack);
+	position: relative;
 
-  .overlay {
-    position: absolute;
-    background-color: var(--ColorBlack);
-    opacity: 0.7;
-    width: 100%;
-    height: 99vh;
-    top: var(--NavbarHeight);
-    left: 0;
-    z-index: 999;
-  }
+	.overlay {
+		position: absolute;
+		background-color: var(--ColorBlack);
+		opacity: 0.7;
+		width: 100%;
+		height: 85vh;
+		top: var(--NavbarHeight);
+		left: 0;
+		z-index: 999;
+	}
 
-  #video {
-    position: absolute;
-    object-fit: cover;
-    top: var(--NavbarHeight);
-    left: 0;
-    width: 100%;
-    height: 85vh;
-  }
+	#video {
+		position: absolute;
+		object-fit: cover;
+		top: var(--NavbarHeight);
+		left: 0;
+		width: 100%;
+		height: 85vh;
+	}
 
-  .banner-img {
-    position: absolute;
-    object-fit: cover;
-    top: var(--NavbarHeight);
-    left: 0;
-    width: 100%;
-    height: 99vh;
-  }
+	.banner-img {
+		position: absolute;
+		object-fit: cover;
+		top: var(--NavbarHeight);
+		left: 0;
+		width: 100%;
+		height: 85vh;
+	}
 
-  .data-container {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    z-index: 1000;
-    color: var(--ColorWhite);
-    font-size: 1rem;
-    font-weight: 100;
-    letter-spacing: 2px;
-    font-family: var(--FontTextLogo);
-    text-align: center;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-between;
-    height: 6rem;
+	.data-container {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		z-index: 1000;
+		color: var(--ColorWhite);
+		font-size: 1rem;
+		font-weight: 100;
+		letter-spacing: 2px;
+		font-family: var(--FontTextLogo);
+		text-align: center;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: space-between;
+		height: 6rem;
 
-    h2 {
-      font-weight: 200;
-      white-space: nowrap;
-    }
-  }
+		h2 {
+			font-weight: 200;
+			white-space: nowrap;
+		}
+	}
 
+	@media screen and (min-width: 980px) {
+		height: 90vh;
 
-  @media screen and (min-width: 980px) {
-    height: 75vh;
+		.overlay {
+			top: var(--NavbarHeight980);
+			height: 90vh;
+		}
 
-    .overlay {
-      top: var(--NavbarHeight980);
-      height: 70vh;
-    }
+		#video {
+			top: var(--NavbarHeight980);
+			height: 90vh;
+		}
 
-    #video {
-      top: var(--NavbarHeight980);
-      height: 70vh;
-    }
-
-    .banner-img {
-      top: var(--NavbarHeight980);
-      height: 70vh;
-    }
-
-  }
-
+		.banner-img {
+			top: var(--NavbarHeight980);
+			height: 90vh;
+		}
+	}
 `;
 export default BannerFullScreenHero;
